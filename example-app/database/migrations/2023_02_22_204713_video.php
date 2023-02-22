da0f4b1c1d64fb144092d01a819093524ab5964f
emirhansonmez <emirhan_sonmez12@outlook.com>
2023-02-23 00:04:25 +0300

    first commit

---
+++ example-app/database/migrations/2023_02_22_204713_video.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Video extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->string('code')->unique();
+            $table->string('name', 255);
+            $table->text('description');
+            $table->string('file', 128);
+            $table->primary('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
